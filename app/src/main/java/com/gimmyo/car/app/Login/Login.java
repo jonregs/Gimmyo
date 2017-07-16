15304e78fc60de4cc8ab17573fce3c83349f10db
jonegalado <regsjondev@gmail.com>
2017-07-15 22:06:50 -0700

    base login page created with some logic, still needs improvement.

--- app/src/main/java/com/gimmyo/car/app/Login/Login.java
+++ app/src/main/java/com/gimmyo/car/app/Login/Login.java
@@ -7,7 +7,7 @@ import com.gimmyo.car.app.R;
 import com.parse.Parse;
 
 /**
- * Created by jonegalado on 7/12/17.
+ * Created by jonegalado on 7/12/17, Gimmyo Project.
  */
 
 public class Login extends AppCompatActivity{
@@ -16,22 +16,22 @@ public class Login extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
 
-        // Enable Local Data Store.
+        /*// Enable Local Data Store.
         Parse.enableLocalDatastore(this);
 
-        /*
+        *//*
          Parse Login Credentials
          URL: http://ec2-18-220-7-92.us-east-2.compute.amazonaws.com/apps/My%20Bitnami%20Parse%20API/browser/_User
          Login to Parse Server
          username: user
          password: j8yiiNOCeurK
-         */
+         *//*
         Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
                 .applicationId("8812ae35cb75fe6d52f018ec0a8add5339b999e8")
                 .clientKey("dc0190dd5f7ca905b849a6ab18d292c4602dec93")
                 .server("http://ec2-18-220-7-92.us-east-2.compute.amazonaws.com:80/parse/")
                 .build()
-        );
+        );*/
     }
 
 }
