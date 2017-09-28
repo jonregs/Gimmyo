5b38571eefe7ab6071374c8642a60acd0d233b39
jonregs <regsjondev@gmail.com>
2017-09-27 18:38:04 -0700

    add_stuff

---
+++ app/src/main/java/com/gimmyo/car/app/utils/ConnectivityManager.java
@@ -0,0 +1,10 @@
+package com.gimmyo.car.app.utils;
+
+/**
+ * Created by Jon on 9/23/2017.
+ */
+
+public class ConnectivityManager {
+
+    
+}
---
+++ app/src/main/java/com/gimmyo/car/app/utils/ServerType.java
@@ -0,0 +1,8 @@
+package com.gimmyo.car.app.utils;
+
+/**
+ * Created by Jon on 9/18/2017.
+ */
+
+public class ServerType {
+}
